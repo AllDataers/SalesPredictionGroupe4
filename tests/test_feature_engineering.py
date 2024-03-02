68de05562d0ee8349c959f2154e07d1828f3a48b
MAMADOU KORKA DIALLO <mkorka96.diallo@gmail.com>
2024-03-02 19:48:24 +0100

    add tests execxution in ci pipeline

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -16,7 +16,7 @@ class TestFeatureEngineering(unittest.TestCase):
             "OrderDate": ["01/01/20 10:00", "02/01/20 11:00"],
         }
         self.address_data = {
-            "address": [
+            "Address": [
                 "123 Apple St, 101, Cupertino, CA, 95014",
                 "456 Banana Ave, 202, Mountain View, CA, 94043",
             ]
@@ -28,35 +28,35 @@ class TestFeatureEngineering(unittest.TestCase):
         # Test DateFeatureEngineering.transform
         transformer = DateFeatureEngineering(date_column_name="OrderDate")
         transformed_df = transformer.transform(self.df_date.copy())
-        self.assertTrue("hour" in transformed_df.columns)
-        self.assertTrue("month" in transformed_df.columns)
-        self.assertTrue("day" in transformed_df.columns)
-        self.assertTrue("year" in transformed_df.columns)
-        self.assertEqual(transformed_df["hour"].iloc[0], 10)
-        self.assertEqual(transformed_df["month"].iloc[0], 1)
-        self.assertEqual(transformed_df["day"].iloc[0], 1)
-        self.assertEqual(transformed_df["year"].iloc[0], 2020)
+        self.assertTrue("Hour" in transformed_df.columns)
+        self.assertTrue("Month" in transformed_df.columns)
+        self.assertTrue("Day" in transformed_df.columns)
+        self.assertTrue("Year" in transformed_df.columns)
+        self.assertEqual(transformed_df["Hour"].iloc[0], 10)
+        self.assertEqual(transformed_df["Month"].iloc[0], 1)
+        self.assertEqual(transformed_df["Day"].iloc[0], 1)
+        self.assertEqual(transformed_df["Year"].iloc[0], 2020)
 
     def test_address_feature_engineering(self):
         # Test AddressFeatureEngineering.transform
-        transformer = AddressFeatureEngineering(address_column_name="address")
+        transformer = AddressFeatureEngineering(address_column_name="Address")
         transformed_df = transformer.transform(self.df_address.copy())
-        self.assertTrue("street_name" in transformed_df.columns)
-        self.assertTrue("street_number" in transformed_df.columns)
-        self.assertTrue("city" in transformed_df.columns)
-        self.assertTrue("state" in transformed_df.columns)
-        self.assertTrue("zip_code" in transformed_df.columns)
-        self.assertEqual(transformed_df["street_name"].iloc[0], "123 Apple St")
-        self.assertEqual(transformed_df["street_number"].iloc[0], "101")
-        self.assertEqual(transformed_df["city"].iloc[0], "Cupertino")
-        self.assertEqual(transformed_df["state"].iloc[0], "CA")
-        self.assertEqual(transformed_df["zip_code"].iloc[0], "95014")
+        self.assertTrue("StreetName" in transformed_df.columns)
+        self.assertTrue("StreetNumber" in transformed_df.columns)
+        self.assertTrue("City" in transformed_df.columns)
+        self.assertTrue("Ctate" in transformed_df.columns)
+        self.assertTrue("ZipCode" in transformed_df.columns)
+        self.assertEqual(transformed_df["StreetName"].iloc[0], "123 Apple St")
+        self.assertEqual(transformed_df["StreetNumber"].iloc[0], "101")
+        self.assertEqual(transformed_df["City"].iloc[0], "Cupertino")
+        self.assertEqual(transformed_df["State"].iloc[0], "CA")
+        self.assertEqual(transformed_df["ZipCode"].iloc[0], "95014")
 
     def test_pipeline(self):
         # Test FeatureEngineeringPipeline with both Date and Address feature engineering
         steps = [
             DateFeatureEngineering(date_column_name="OrderDate"),
-            AddressFeatureEngineering(address_column_name="address"),
+            AddressFeatureEngineering(address_column_name="Address"),
         ]
         pipeline = FeatureEngineeringPipeline(logger=self.logger,
                                               feature_engineering_steps=steps)
@@ -64,15 +64,15 @@ class TestFeatureEngineering(unittest.TestCase):
         transformed_df, _ = pipeline.transform(df_combined.copy())
         # Check if all expected columns are present
         expected_columns = [
-            "hour",
-            "month",
-            "day",
-            "year",
-            "street_name",
-            "street_number",
-            "city",
-            "state",
-            "zip_code",
+            "Hour",
+            "Month",
+            "Day",
+            "Year",
+            "StreetName",
+            "StreetNumber",
+            "City",
+            "State",
+            "ZipCode",
         ]
         for column in expected_columns:
             self.assertTrue(column in transformed_df.columns)
