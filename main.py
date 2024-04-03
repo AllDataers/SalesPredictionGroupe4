3e85382cafe291c7388293b2fe06071c628b03b3
CerateAGNIKPE <125263974+CerateAGNIKPE@users.noreply.github.com>
2024-04-03 14:56:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
+import folium
+from folium import plugins
+from folium.plugins import HeatMap
+import geocoder
+import geopy
+from streamlit_folium import st_folium
 
 def get_df_by_city(df: pd.DataFrame, cities: list):
     return df[df['CityName'].isin(cities)]
@@ -48,3 +53,39 @@ df_cityQ = df_cityQ.sort_values(by='QuantityOrdered', ascending=True)
 fig = px.bar(df_cityQ, y='CityName', x='QuantityOrdered', title='Quantity Ordered by City',
              orientation='h', color='QuantityOrdered', color_continuous_scale=px.colors.sequential.Emrld)
 st.plotly_chart(fig)
+
+citySales = df.groupby(['CityName']).agg({'QuantityOrdered' : 'sum',  'Sales' : 'sum'}).reset_index()
+
+def create_heatmap(citylist, citySales):
+    mp = folium.Map(location=[41, -99], zoom_start=3.5,  title='City Sales Heatmap')  # Initial center coordinates
+
+    loc = []
+    for i in citylist:
+        try:
+            geo = geocoder.arcgis(i)  # Attempt to geocode the city name
+            val = citySales.loc[citySales['CityName'] == i]['Sales'].item()
+            loc.append([geo.lat, geo.lng, val])
+
+            # Create marker with proper tooltip formatting
+            folium.Marker([geo.lat, geo.lng],
+                          opacity=0,
+                          tooltip=f'<body>City: {i} <br> Sales: {round(val / 1000000, 3)}M</body>'
+                          ).add_to(mp)
+        except (geocoder.GeocoderQuotaDepleted, geocoder.GeocoderTimedOut, KeyError):
+            st.error(f"Error geocoding city: {i}. Skipping...")
+
+    # Add heatmap with appropriate weighting based on sales values
+    HeatMap(loc, weight_len=3).add_to(mp)
+
+    folium.LayerControl().add_to(mp)
+    return mp
+
+# Load your city list and sales data from a DataFrame (replace with your data source)
+citylist = df.CityName.unique().tolist()
+
+
+st.write("""
+<b>City Sales Heatmap</b>
+""", unsafe_allow_html=True)
+# Display the heatmap using Streamlit
+st_folium(create_heatmap(citylist, citySales), width=1500)
